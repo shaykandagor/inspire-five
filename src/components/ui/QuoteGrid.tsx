0019a763ba46034d3736d86f85fd712424a17b79
Shayne Kandagor <shaynekandagor@gmail.com>
2025-05-14 06:06:23 +0300

    Implement author search functionality in QuoteGrid component and update layout with Card components

--- src/components/ui/QuoteGrid.tsx
+++ src/components/ui/QuoteGrid.tsx
@@ -1,13 +1,22 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Card, Input, SimpleGrid, Text, Image } from "@chakra-ui/react";
 import useQuotes from "../../hooks/useQuotes";
-import QuoteCard from "./QuoteCard";
-import React from "react";
+import React, { useState } from "react";
 
 const QuoteGrid = () => {
   const { quotes, error, loading } = useQuotes();
+  const [authorSearch, setAuthorSearch] = useState("");
+  const filteredQuotes = quotes.filter((quote) =>
+    quote.a.toLowerCase().includes(authorSearch.toLowerCase())
+  );
   return (
     <>
-      <p>{quotes.length}</p>
+      <Input
+        placeholder="Search author..."
+        onChange={(e) => {
+          setAuthorSearch(e.target.value);
+        }}
+        value={authorSearch}
+      />
       <SimpleGrid
         captionSide={"top"}
         columns={{ base: 1, md: 2, lg: 3, xl: 4 }}
@@ -16,8 +25,31 @@ const QuoteGrid = () => {
         {loading && <Text>Loading...</Text>}
         {quotes.length === 0 && !error && <Text>No quotes found</Text>}
         {error && <Text>{error}</Text>}
-        {quotes.map((quote, index) => (
-          <QuoteCard key={index} quote={quote} />
+        {filteredQuotes.map((quote, index) => (
+          <Card.Root
+            key={index}
+            flexDirection={"row"}
+            overflow="hidden"
+            maxWidth="xl"
+            borderRadius={8}
+            padding={5}
+            margin={5}
+          >
+            <Image
+              objectFit="cover"
+              maxWidth="200px"
+              alt="Author Avatar"
+              src={quote.i}
+            />
+            <Box>
+              <Card.Header fontSize={"2xl"} fontWeight="bold">
+                <Card.Title>{quote.a}</Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <Card.Header>{quote.q}</Card.Header>
+              </Card.Body>
+            </Box>
+          </Card.Root>
         ))}
       </SimpleGrid>
     </>
